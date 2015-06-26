5f7bfcbaa6942e3d495d5c0212f5a756ad23464a
Rob Day <Robert.Day@metaswitch.com>
2015-06-26 14:15:17 +0000

    Switch to host templates

--- cookbooks/clearwater/files/default/cacti/add_device.sh
+++ cookbooks/clearwater/files/default/cacti/add_device.sh
@@ -5,10 +5,20 @@ name=$2
 tmpl_name=$3
 
 cd /usr/share/cacti/cli
+
+# Get the template number from the name
 tmpl=`./add_device.php --list-host-templates | grep $tmpl_name | cut -f1`
+
+# Add the host, associating it with the appropriate template
 ./add_device.php --ip=$ip --description=$name --community=clearwater --template=$tmpl --avail=snmp
+
+# Find the ID of this host
 this_node=`./add_graphs.php --list-hosts | grep $name | cut -f1`
+
+# Add an entry for this host to the graphs tree
 ./add_tree.php --type=node --node-type=host --host-id=$this_node --tree-id=1
+
+# Add each graph for the host
 for graph in `./add_graphs.php --list-graph-templates --host-template-id=$tmpl | grep -E "^[0-9]" |
   cut -f 1`
 do
---
+++ plugins/knife/knife-cacti-update.rb
@@ -0,0 +1,73 @@
+# @file knife-cacti-update.rb
+#
+# Project Clearwater - IMS in the Cloud
+# Copyright (C) 2015 Metaswitch Networks Ltd
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version, along with the "Special Exception" for use of
+# the program along with SSL, set forth below. This program is distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details. You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# The author can be reached by email at clearwater@metaswitch.com or by
+# post at Metaswitch Networks Ltd, 100 Church St, Enfield EN2 6BQ, UK
+#
+# Special Exception
+# Metaswitch Networks Ltd  grants you permission to copy, modify,
+# propagate, and distribute a work formed by combining OpenSSL with The
+# Software, or a work derivative of such a combination, even if such
+# copying, modification, propagation, or distribution would otherwise
+# violate the terms of the GPL. You must comply with the GPL in all
+# respects for all of the code used other than OpenSSL.
+# "OpenSSL" means OpenSSL toolkit software distributed by the OpenSSL
+# Project and licensed under the OpenSSL Licenses, or a work based on such
+# software and licensed under the OpenSSL Licenses.
+# "OpenSSL Licenses" means the OpenSSL License and Original SSLeay License
+# under which the OpenSSL Project distributes the OpenSSL toolkit software,
+# as those licenses appear in the file LICENSE-OPENSSL.
+
+require_relative 'knife-clearwater-utils'
+require_relative 'trigger-chef-client'
+
+module ClearwaterKnifePlugins
+  class CactiUpdate < Chef::Knife
+    include ClearwaterKnifePlugins::ClearwaterUtils
+    include ClearwaterKnifePlugins::TriggerChefClient
+
+    deps do
+      require 'chef'
+      require 'fog'
+    end
+
+    banner "cacti update"
+
+    def run()
+
+      # For each Bono, Sprout and SIPp node, set it up in Cacti and associate it with the
+      # appropriately-named host template
+
+      find_nodes(roles: "clearwater-infrastructure", role: "cacti").each do |cacti|
+        find_nodes(roles: "clearwater-infrastructure", role: "bono").each do |node|
+          run_command(options[:cloud], "chef_environment:#{env} AND name:#{cacti.name}", "sudo bash /usr/share/clearwater/cacti/add_device.sh #{node.cloud.local_ipv4} #{node.name} Bono")
+        end
+
+        find_nodes(roles: "clearwater-infrastructure", role: "sprout").each do |node|
+          run_command(options[:cloud], "chef_environment:#{env} AND name:#{cacti.name}", "sudo bash /usr/share/clearwater/cacti/add_device.sh #{node.cloud.local_ipv4} #{node.name} Sprout")
+        end
+
+        find_nodes(roles: "clearwater-infrastructure", role: "sipp").each do |node|
+          run_command(options[:cloud], "chef_environment:#{env} AND name:#{cacti.name}", "sudo bash /usr/share/clearwater/cacti/add_device.sh #{node.cloud.local_ipv4} #{node.name} SIPp")
+        end
+
+        end
+      end
+
+    end
+  end
+end
